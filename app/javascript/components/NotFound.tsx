f0cb91ba6719546cdb4676f12001a63cce52a00d
clay <clay@clays-MacBook-Air.local>
2024-05-30 16:11:33 +0700

    init project

---
+++ app/javascript/components/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+      <p className="lead">Not Found!</p>
+    </div>
+  );
+}
