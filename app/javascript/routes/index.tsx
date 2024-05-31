4baa99309a59787723711eff93c6524277b67056
clay <clay@clays-MacBook-Air.local>
2024-05-31 13:41:02 +0700

    add crud

--- app/javascript/routes/index.tsx
+++ app/javascript/routes/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
+import Recipes from "../components/Recipes";
+import Recipe from "../components/Recipe";
+import NewRecipe from "../components/NewRecipe";
 import NotFound from "../components/NotFound";
 
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/recipe/:id" element={<Recipe />} />
+      <Route path="/recipe" element={<NewRecipe />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+9.times do |i|
+  Recipe.create(
+    name: "Recipe #{i + 1}",
+    ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+    instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
+  )
+end
